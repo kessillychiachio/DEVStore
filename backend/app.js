81c25b94fff47b709c947efaf1966738fa7b49ef
Ana Chiachio <kessilly_chiachio@hotmail.com>
2025-02-23 15:10:31 -0300

    alt no backend

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,15 @@
-const express = require("express")
-const rotaLivro = require("./rotas/livro")
-const rotaFavorito = require("./rotas/favoritos")
+const express = require("express");
+const cors = require("cors");
 
-const cors = require("cors")
+const rotaLivro = require("./rotas/livro");
+const rotaFavorito = require("./rotas/favoritos");
 
-const app = express()
-app.use(express.json())
-app.use(cors({origin: "*"}))
+const app = express();
 
-app.use('/livros', rotaLivro)
-app.use('/favoritos', rotaFavorito)
+app.use(express.json());
+app.use(cors({ origin: "*" }));
 
-const port = 8000
+app.use("/livros", rotaLivro);
+app.use("/favoritos", rotaFavorito);
 
-app.listen(port, () => {
-    console.log(`Escutando a porta ${port}`)
-  })
+module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,8 @@
 const express = require("express");
-const cors = require("cors");
-const app = require("./app"); 
+const app = require("./app");
 
 const PORT = process.env.PORT || 8000;
 
-const server = express();
-
-server.use(cors()); 
-server.use(express.json()); 
-
-
-server.use(app);
-
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
 });
